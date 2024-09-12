c85237fa66991b2be39a765b6d5e8cf3e0f73855
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-12 13:44:08 +0200

    added zod

--- app/app/ui/components/AddTask.jsx
+++ app/app/ui/components/AddTask.jsx
@@ -4,23 +4,35 @@ import { Button } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { v4 as uuidv4 } from "uuid";
 
-import { TaskList } from "./TaskList";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const schema = z
+  .object({
+    name: z.string().min(3),
+  })
+  .required();
 
 export const AddTask = () => {
   const {
     register,
     formState: { errors },
     handleSubmit,
-  } = useForm();
+  } = useForm({ resolver: zodResolver(schema) });
 
   const [tasks, setTasks] = useState([]);
-  const [taskDone, setTaskDone] = useState([]);
+  const [tasksDone, setTasksDone] = useState([]);
 
   console.log(tasks);
 
   const onSubmit = (data) => {
-    const newTask = { name: data.name, id: uuidv4(), done: false };
+    const newTask = { name: data.name, id: uuidv4() };
     setTasks([...tasks, newTask]);
+  };
+
+  const handleTaskDone = (taskDone) => {
+    setTasks(tasks.filter((task) => task.id !== taskDone.id));
+    setTasksDone([...tasksDone, taskDone]);
   };
 
   return (
@@ -35,12 +47,42 @@ export const AddTask = () => {
             backgroundColor: "white",
           }}
           fullWidth
+          error={!!errors.name}
+          helperText={errors.name && errors.name.message}
         />
         <Button variant="contained" color="primary" type="submit">
           Add
         </Button>
       </Box>
-      {/* <ul>{tasks.map(task => (<li key={task.id}>task.name</li>))}</ul> */}
+      <ul>
+        <br />
+        TaskList:
+        {tasks.map((task) => (
+          <div
+            style={{ display: "flex", justifyContent: "space-between" }}
+            key={task.id}
+          >
+            <li>{task.name}</li>
+            <div>
+              {" "}
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => handleTaskDone(task)}
+              >
+                Done
+              </Button>
+            </div>
+          </div>
+        ))}
+      </ul>
+      <div style={{ width: "350px", borderTop: "1px solid white" }} />
+      <ul>
+        Done:
+        {tasksDone.map((taskDone) => (
+          <li key={taskDone.id}>{taskDone.name}</li>
+        ))}
+      </ul>
     </>
   );
 };
